7a8f6c2fdac7d51e94024a963197aa2954200550
xiaoxion <esaurubio@fullsail.edu>
2012-11-29 18:36:21 -0500

    add String function, and cleaned up numerical function.

--- Rubio_Esau_assignment2.js
+++ Rubio_Esau_assignment2.js
@@ -4,11 +4,9 @@
 // Meeting with Supervisor
 
 // Initial Variables
-var supervisorName = "Ray"                                ,
-    managerName    = "Esau"                               ,
-    jobName        = "AT&T Job"                           ,
-	footageCount   = 986                                  ,
-	contractorNames = [ "Diggy" , "Driller" , "Dugger" ]
+var supervisorName  = "Ray"                                ,
+    managerName     = "Esau"                               ,
+    jobName         = "AT&T Job"
 ;
 
 // Constant Variables
@@ -20,7 +18,7 @@ var say = function (what) {
 
 // Procedure
 var meetUp = function (response) {
-	say("Hey " + managerName + " can you meet me at " + jobName + "?" );
+	say("Hey, " + managerName + " can you meet me at " + jobName + "?" );
 	if (response === true) {
 		say( managerName + ' says "Yeah, I will be there in a few!"' );
 		say( managerName + ' arrives and asks "What do you need?"');
@@ -30,7 +28,7 @@ var meetUp = function (response) {
 };
 
 // Boolean
-var doJobNextWeek = function (canI) {
+var doJobNextWeek = function (canI , footageCount) {
 	say( supervisorName + ' says "Well, I need a job with ' + footageCount + ' feet by next week. Think you can do it?"' );
 	if (canI === true) {
 		say( managerName + ' says "Sure! We should be able to do that!"' );
@@ -43,28 +41,47 @@ var doJobNextWeek = function (canI) {
 
 // Number
 var quotedPrice = function (howMuch) {
+	var otherPrices = [ 8.00 , 9.00 , 9.75 ]
 	if ( canIDoIt === true ) {
 		say( supervisorName + ' says "That\'s great! What is the price per foot?"' );
-		say( managerName + ' says "$' + howMuch + ' per foot sounds about right for this job."' );
-		if ( howMuch < 8.00 ) {
-			say( supervisorName + ' says "Sounds good to me!"')
-		} else {
-			say( supervisorName + ' says "That\'s a little high for this job.')
+		say( managerName + ' says "How about $' + howMuch + ' per foot."' );
+		while ( howMuch > otherPrices[0] ) {
+			say( supervisorName + ' says "That\'s a little high. Can you lower it?')
+			howMuch--
+			say( managerName + ' says "How about $' + howMuch + ' per foot."' );
 		};
+		say( supervisorName + ' says "Sounds good to me!"')
 	};
 	howMuchIsIt = howMuch
 	return howMuchIsIt
 };
 
 // String
-
+var cheapGuys = function ( guy1 , guy2 ) {
+	if ( canIDoIt === true ) {
+		var randomnumber = Math.floor(Math.random()*2)
+		if ( randomnumber === 0 ) {
+			say( supervisorName + ' says "Whoops! Hold on ' + guy1 + ' is calling me back from earlier."' );
+			var string = say( supervisorName + ' says "Whoa, sorry ' + guy1 + ' undercut you with $' + (howMuchIsIt + 1 ) +'."');
+			string;
+		} else {
+			say( supervisorName + ' says "Whoops! Hold on ' + guy2 + ' is calling me back from earlier."' );
+			var string = say( supervisorName + ' says "Whoa, ' + guy2 + ' wanted the job at $' + (howMuchIsIt - 1 ) +'!"');
+			string;
+		};
+	};
+	return string
+};
 
 // Array
+var otherContractors = function () {
 
+};
 
 // Output
 
 meetUp( true );
-doJobNextWeek( true );
-quotedPrice( 7.75 );
-
+doJobNextWeek( true , 986 );
+quotedPrice( 8.75 );
+cheapGuys( "Jason" , "Bob" );
+otherContractors([ "Diggy" , "Driller" , "Dugger" ])
