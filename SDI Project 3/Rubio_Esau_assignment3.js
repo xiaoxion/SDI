96616fa9a59fbc522ab52ec9c683340f99a9c7d4
xiaoxion <esaurubio@fullsail.edu>
2012-12-06 22:08:14 -0500

    finished objects and code clean-up

--- SDI Project 3/Rubio_Esau_assignment3.js
+++ SDI Project 3/Rubio_Esau_assignment3.js
@@ -10,7 +10,7 @@ var supervisorName     = "Ray"                                  ,
 	footageCount       = 986                                    ,
 	say                = function (text) {console.log(text)}    ;
 
-// Main Object
+// How he decides who he is getting
 var whoToGet = {
 	"loop": function () {
 		for (var key in otherCompanys.companys ) {
@@ -23,14 +23,17 @@ var whoToGet = {
 			if (comp.price < 6) {
 				say( supervisorName + ' says "Whoa! ' + comp.name + ' can do it at $' + comp.price + '! He\'s got the job!"');
 				var gotByDefault = true;
+				return [ gotByDefault , comp.price ]
 			};
+			var gotByDefault = false
+			return gotByDefault
 		};
 		return gotByDefault
 	}, // jobByDefault
 	"recapOfCompany": function (comp) {
 		if (comp.canDoJob === true) {
 			say( supervisorName + ' thinks to himself "Hmm... ' + comp.name + ' Underground can do the job for $' + comp.price + '!"' );
-			if (comp.price < 8) {
+			if (comp.price < supervisorMaxPrice) {
 				say( supervisorName + ' thinks "Well that is a reasonable price."')
 			} else {
 				say( supervisorName + ' thinks "Maybe I can get him to lower it."')
@@ -46,8 +49,29 @@ var whoToGet = {
 			whoToGet.recapOfCompany(comp);
 		};
 		return comp
-	}, //whichCompanys
+	}, // whichCompanys
+	"lowerPrice": function (otherCompanys) {
+		var maxPrice = supervisorMaxPrice,
+			minPrice = 6,
+			preference = "Best";
+		for ( var key in otherCompanys.companys ) {
+			var comp = otherCompanys.companys[key]
+			if ( comp.price > maxPrice ) {
+				while(comp.price > maxPrice && comp.price > comp.minMax[0]) {
+					say( supervisorName + ' says "I wonder if ' + comp.name + ' will lower his price. Let me ask him!"');
+					comp.price-=0.5
+					say( supervisorName + ' says "Well. It looks like $' + comp.price + ' is the final price for ' + comp.name + '."');
+					return comp.price;
+				};
+			} else {
+				var fine = say( supervisorName + ' says "Well I think ' + comp.name + ' price is fine where it stands.')
+				fine
+				return fine;
+			};
+		};
+	}
 };
 
 whoToGet.jobByDefault(otherCompanys);
 whoToGet.whichCompanys(otherCompanys);
+whoToGet.lowerPrice(otherCompanys);
